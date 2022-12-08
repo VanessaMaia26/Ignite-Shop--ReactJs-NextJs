8dc1741d4e2c2c1b5b0e5e9a531aad13864c134c
VanessaMaia26 <vanessamaia2607@gmail.com>
2022-12-08 13:52:53 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app';
+
+import Image from 'next/image';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
+  return (
+    <Container>
+      <Header> 
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/Home";
+
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+    </HomeContainer>
+  )
+}
